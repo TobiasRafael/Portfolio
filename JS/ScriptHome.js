c59c04905c507091048295d0aff8e6be9f1590b7
TobiasRafael <201422211@unapec.edu.do>
2017-05-04 16:59:40 -0400

    Bootstrap Modals added

--- JS/ScriptHome.js
+++ JS/ScriptHome.js
@@ -1,10 +1,25 @@
 function theNav() {
 
-    var x = document.getElementById("nav");
+    var x = document.getElementById("navvv");
 
-    if (x.className === "navbar") {
+    if (x.className === "navv") {
         x.className += " responsive";
     } else {
-        x.className = "navbar";
+        x.className = "navv";
     }
+}
+
+var one = document.getElementById("first");
+var two = document.getElementById("second");
+var three = document.getElementById("third");
+
+one.onclick = function() {
+    $('#one').modal('show');
+}
+
+two.onclick = function() {
+    $('#two').modal('show');
+}
+three.onclick = function() {
+    $('#three').modal('show');
 }
